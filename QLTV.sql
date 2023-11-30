3cf6ef6030d292e6f80f53bef2ebfab022fecabc
vinhp03 <118117621+vinhp03@users.noreply.github.com>
2023-11-30 15:48:28 +0700

    QLTV.sql

--- QLTV.sql
+++ QLTV.sql
@@ -2,7 +2,7 @@
 use QLTV
 go
 CREATE TABLE Sach (
-    Ma_sach VARCHAR(255),
+    Ma_sach VARCHAR(255) PRIMARY KEY,
     Ten_sach NVARCHAR(255),
     The_loai NVARCHAR(255),
     So_luong int,
@@ -11,17 +11,34 @@ CREATE TABLE Sach (
     Mo_ta NVARCHAR(255)
 );
 
-create table Thong_tin_nguoi_muon (
-	Ma_doc_gia varchar(255),
+CREATE TABLE Thong_tin_doc_gia (
+	Ma_doc_gia varchar(255) PRIMARY KEY,
 	Ten_tai_khoan varchar(255),
 	Mat_khau varchar(255),
 	Ho_ten nvarchar(255),
 	So_dien_thoai text,
 	Phuong_Xa nvarchar(255),
 	Quan_huyen nvarchar(255),
-	Tinh nvarchar(255)
+	Tinh nvarchar(255),
+	Email nvarchar(255)
 );
--- Bổ sung dữ liệu cho bảng SACH sử dụng GETDATE() trong SQL Server
+
+CREATE TABLE Nguoi_muon (
+	Ma_doc_gia varchar(255),
+	Ma_sach VARCHAR(255),
+	Ngay_muon date,
+	Ngay_tra date,
+	FOREIGN KEY (Ma_doc_gia) REFERENCES Thong_tin_doc_gia(Ma_doc_gia),
+	FOREIGN KEY (Ma_sach) REFERENCES Sach(Ma_sach)
+);
+
+/*INSERT INTO Nguoi_muon (Ma_doc_gia, Ma_sach, Ngay_muon, Ngay_tra) Values ('MD001', 'MS001', GETDATE(), '2023/10/15');*/
+	
+create table TK_QL (
+	Ten_tai_khoan varchar(255),
+	Mat_khau varchar(255)
+);
+
 INSERT INTO Sach (Ma_sach, Ten_sach, The_loai, So_luong, Tinh_trang, Thoi_gian_nhap_kho, Mo_ta)
 VALUES
     ('MS001', N'Dấu chân trên cát', N'Tiểu thuyết', 6, N'Mới', GETDATE(), NULL),
@@ -39,8 +56,8 @@ VALUES
     ('MS013', N'Chiến lược dành cho người cận thị', N'Hồi ký', 1, N'Cũ', GETDATE(), NULL),
     ('MS014', N'Truyện Kiều', N'Thơ', 6, N'Mới', GETDATE(), NULL),
     ('MS015', N'Cẩm nang thương trường', N'Quản lý kinh doanh', 5, N'Cũ', GETDATE(), NULL);
--- Bổ sung dữ liệu cho bảng Thong_tin_nguoi_muon
-INSERT INTO Thong_tin_nguoi_muon (Ma_doc_gia, Ten_tai_khoan, Mat_khau, Ho_ten, So_dien_thoai, Phuong_Xa, Quan_huyen, Tinh)
+
+INSERT INTO Thong_tin_doc_gia(Ma_doc_gia, Ten_tai_khoan, Mat_khau, Ho_ten, So_dien_thoai, Phuong_Xa, Quan_huyen, Tinh)
 VALUES
     ('MD001', 'nguoimuon1', 'matkhau1', N'Người Mượn 1', '0123456789', N'Phường Bùi Thị Xuân', N'Quận Thành phố Quy Nhơn', N'Bình Định'),
     ('MD002', 'nguoimuon2', 'matkhau2', N'Người Mượn 2', '0987654321', N'Phường Đống Đa', N'Quận Thành phố Quy Nhơn', N'Bình Định'),
@@ -49,7 +66,6 @@ VALUES
     ('MD005', 'nguoimuon5', 'matkhau5', N'Người Mượn 5', '0765432109', N'Phường Lê Hồng Phong', N'Quận Thành phố Quy Nhơn', N'Bình Định'),
     ('MD006', 'nguoimuon6', 'matkhau6', N'Người Mượn 6', '0918273645', N'Phường Lê Lợi', N'Quận Thành phố Quy Nhơn', N'Bình Định'),
     ('MD007', 'nguoimuon7', 'matkhau7', N'Người Mượn 7', '0587963210', N'Phường Lý Thường Kiệt', N'Quận Thành phố Quy Nhơn', N'Bình Định');
-ALTER TABLE Sach
-ALTER COLUMN Ma_sach VARCHAR(255) NOT NULL;
-ALter table Thong_tin_nguoi_muon
-Alter column Ma_doc_gia varchar(255) not null;
+
+INSERT INTO TK_QL (Ten_tai_khoan, Mat_khau) values ('admin', '123');
+
