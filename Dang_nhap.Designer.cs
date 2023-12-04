b8ce33ee49243b60ef42aab6932cf731cb419057
TuanPhong20 <144501592+TuanPhong20@users.noreply.github.com>
2023-12-04 17:24:17 +0700

    Update Dang_nhap.Designer.cs

--- Dang_nhap.Designer.cs
+++ Dang_nhap.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Quan_ly_thu_vien
+namespace Quan_ly_thu_vien
 {
     partial class Dang_nhap
     {
@@ -31,15 +31,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(156, 213);
+            this.button1.Location = new System.Drawing.Point(145, 268);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 33);
             this.button1.TabIndex = 0;
@@ -77,25 +77,6 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Mật khẩu ";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(98, 276);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Chưa có tài khoản?";
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(207, 276);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(75, 13);
-            this.linkLabel1.TabIndex = 5;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Đăng kí ngay ";
-            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(156, 107);
@@ -110,22 +91,42 @@
             this.textBox2.Size = new System.Drawing.Size(185, 20);
             this.textBox2.TabIndex = 7;
             // 
-            // Form1
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(69, 215);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Admin";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(228, 222);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "User";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Dang_nhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            /*this.BackgroundImage = global::baitapnhom.Properties.Resources.ke_sach_mau_trang_1m5;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;*/
             this.ClientSize = new System.Drawing.Size(384, 355);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "Dang_nhap";
             this.Text = "Đăng Nhập ";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,9 +139,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
