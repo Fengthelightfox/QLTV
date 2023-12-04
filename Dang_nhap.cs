7f49834758e9d462b555e7ae45889a4233db09c1
TuanPhong20 <144501592+TuanPhong20@users.noreply.github.com>
2023-12-04 17:24:40 +0700

    Update Dang_nhap.cs

--- Dang_nhap.cs
+++ Dang_nhap.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,31 +25,54 @@ namespace Quan_ly_thu_vien
             try
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("select * from TK_QL where Ten_tai_khoan = @dn", conn);
-                cmd.Parameters.AddWithValue("@dn", textBox1.Text.Trim());
-                SqlDataReader rd = cmd.ExecuteReader();
-                while (rd.Read())
+                if (radioButton1.Checked)
                 {
-                    if ((textBox1.Text.Trim() == rd["Ten_tai_khoan"].ToString().Trim()) && textBox2.Text.Trim() == rd["Mat_khau"].ToString().Trim())
+                    SqlCommand cmd = new SqlCommand("select * from TK_QL where Ten_tai_khoan = @dn", conn);
+                    cmd.Parameters.AddWithValue("@dn", textBox1.Text.Trim());
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
                     {
-                        MessageBox.Show("Đăng nhập thành công");
-                        this.Hide();
-                        Trang_chu tc = new Trang_chu();
-                        tc.ShowDialog();
-                    }
-                    if (textBox1.Text.ToString() == "" || textBox2.Text.ToString() == "")
-                    {
-                        MessageBox.Show("Vui lòng điền đủ các thông tin!");
-                    }
-                    if (textBox1.Text.Trim() != rd["Ten_tai_khoan"].ToString().Trim())
-                    {
-                        MessageBox.Show("Tài khoản không tồn tại!");
-                    }
-                    if (textBox2.Text.Trim() != rd["Mat_khau"].ToString().Trim())
-                    {
-                        MessageBox.Show("Sai mật khẩu.");
+                        if ((textBox1.Text.Trim() == rd["Ten_tai_khoan"].ToString().Trim()) && textBox2.Text.Trim() == rd["Mat_khau"].ToString().Trim())
+                        {
+                            MessageBox.Show("Đăng nhập thành công");
+                            this.Hide();
+                            Trang_chu tc = new Trang_chu();
+                            tc.ShowDialog();
+                        }
+                        if (textBox1.Text.ToString() == "" || textBox2.Text.ToString() == "")
+                        {
+                            MessageBox.Show("Vui lòng điền đủ các thông tin!");
+                        }
+                        if (textBox1.Text.Trim() != rd["Ten_tai_khoan"].ToString().Trim())
+                        {
+                            MessageBox.Show("Tài khoản không tồn tại!");
+                        }
+                        if (textBox2.Text.Trim() != rd["Mat_khau"].ToString().Trim())
+                        {
+                            MessageBox.Show("Sai mật khẩu.");
+                        }
                     }
                 }
+                if(radioButton2.Checked)
+                {
+                    SqlCommand cmd2 = new SqlCommand("select * from  Thong_tin_doc_gia where Ten_tai_khoan=@dn, Mat_khau=@mk ",conn);
+                    cmd2.Parameters.AddWithValue("@dn", textBox1.Text.Trim());
+                    cmd2.Parameters.AddWithValue("@mk", textBox2.Text.Trim());
+                    SqlDataReader rd = cmd2.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        if ((textBox1.Text.Trim() == rd["Ten_tai_khoan"].ToString().Trim()) && textBox2.Text.Trim() == rd["Mat_khau"].ToString().Trim())
+                        {
+                            MessageBox.Show("Đăng nhập thành công");
+                            this.Hide();
+                            Tim_kiem tk = new Tim_kiem();
+                            tk.ShowDialog();
+                        }
+
+                    }
+                }
+
+
             }
             catch (Exception ex)
             {
